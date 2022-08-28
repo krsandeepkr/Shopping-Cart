f974ffb0db34e9668c9bcdf91dc0c7f8c09060bc
krsandeepkr <106463436+krsandeepkr@users.noreply.github.com>
2022-08-28 11:02:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Cart from "./components/Cart"
-//import Increment from './Increment'
-//import Datentime from './Datentime'
-//import Time from './Time'
+
 
 const App = () => {
   return (
